edad4bb7ddd0de886648d372354ca65e6401e2b2
Sergey Dvornirov <30415511@mail.ru>
2024-07-17 21:43:47 +0500

    Homevork

---
+++ tasks/task_1/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 1: Задайте значения M и N. Напишите программу,
+//  которая выведет все натуральные числа в промежутке от M до N.
+//  Использовать рекурсию, не использовать циклы.
+
+
+Console.Write("Введите значение M: ");
+ int M = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение N: ");
+ int N = Convert.ToInt32(Console.ReadLine());
+
+    PrintNaturalNumbers(M, N); 
+ 
+    void PrintNaturalNumbers(int current, int N) 
+    { 
+        if (current <= N) 
+        { 
+            Console.Write(current + " "); 
+            PrintNaturalNumbers(current + 1, N);   
+        }                  
+    } 
---
+++ tasks/task_3/Program.cs
@@ -0,0 +1,18 @@
+﻿// Задача 3: Задайте произвольный массив. Выведете его элементы, начиная с конца. 
+//      Использовать рекурсию, не использовать циклы.
+ 
+
+        int[] myArray = { 10, 12, 6, 2, 9, 11, 16 }; 
+ 
+        Console.WriteLine("Элементы массива, начиная с конца:"); 
+        PrintArrayReverse(myArray, myArray.Length - 1); 
+
+
+     void PrintArrayReverse(int[] arr, int index) 
+    { 
+        if (index >= 0) 
+        { 
+            Console.Write(arr[index] + " "); 
+            PrintArrayReverse(arr, index - 1); 
+        } 
+    } 
